3bfba368602adb3c03b39c80cb3d86551c6109c7
Me1td0wn76 <jyoriku0103@gmail.com>
2026-06-19 10:12:41 +0900

    feat: redesign CallStaffPage layout and improve user interaction prompts

--- Mos-front/src/pages/CallStaffPage.jsx
+++ Mos-front/src/pages/CallStaffPage.jsx
@@ -1,17 +1,25 @@
 import { Link } from 'react-router-dom'
 import { MenuLayout } from '../components/MenuLayout'
+import '../App.css'
 import '../menu.css'
 
 export default function CallStaffPage() {
   return (
     <MenuLayout activeTab="call">
-      <div className="modal-overlay">
-        <div className="modal-card">
-          <p>店員を呼び出しますか？</p>
-          <div className="modal-actions">
-            <Link to="/call-staff-calling" className="modal-button">呼び出す</Link>
-            <Link to="/menu" className="modal-button is-dark">キャンセル</Link>
-          </div>
+      <div className="call-staff-screen">
+        <div className="call-staff-card">
+          <h2 className="call-staff-title">店員の呼び出し</h2>
+          <p className="call-staff-desc">
+            ご用の際はボタンを押してください。
+            <br />
+            スタッフがすぐに参ります。
+          </p>
+          <Link to="/call-staff-calling" className="call-staff-btn">
+            店員を呼ぶ
+          </Link>
+          <Link to="/menu" className="call-back-btn">
+            キャンセル
+          </Link>
         </div>
       </div>
     </MenuLayout>
--- Mos-front/src/pages/CallingStaffPage.jsx
+++ Mos-front/src/pages/CallingStaffPage.jsx
@@ -1,20 +1,23 @@
 import { Link } from 'react-router-dom'
 import { MenuLayout } from '../components/MenuLayout'
+import '../App.css'
 import '../menu.css'
 
 export default function CallingStaffPage() {
   return (
     <MenuLayout activeTab="call">
-      <div className="calling-screen">
-        <div className="calling-card">
-          <div className="calling-animation">
-            <div className="calling-dot"></div>
-            <div className="calling-dot"></div>
-            <div className="calling-dot"></div>
-          </div>
-          <h2>只今呼び出し中...</h2>
-          <p>店員が参ります。お待ちください。</p>
-          <Link to="/menu" className="calling-button">戻る</Link>
+      <div className="call-staff-screen">
+        <div className="call-staff-card">
+          <div className="calling-staff-icon">🔔</div>
+          <p className="calling-staff-msg">スタッフを呼び出し中</p>
+          <p className="calling-staff-sub">
+            只今スタッフが向かっております。
+            <br />
+            しばらくお待ちください。
+          </p>
+          <Link to="/menu" className="call-back-btn">
+            メニューに戻る
+          </Link>
         </div>
       </div>
     </MenuLayout>
